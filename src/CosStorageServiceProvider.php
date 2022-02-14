c1228e18ba5fb16ff1ede7db62cdbe5bf857e791
overtrue <anzhengchao@gmail.com>
2022-02-14 12:31:11 +0800

    PHP-CS-Fixer v3

--- src/CosStorageServiceProvider.php
+++ src/CosStorageServiceProvider.php
@@ -1,24 +1,12 @@
 <?php
 
-/*
- * This file is part of the overtrue/laravel-filesystem-cos.
- * (c) overtrue <i@overtrue.me>
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 namespace Overtrue\LaravelFilesystem\Cos;
 
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Filesystem;
 use Overtrue\Flysystem\Cos\CosAdapter;
-use Overtrue\Flysystem\Cos\Plugins\FileSignedUrl;
-use Overtrue\Flysystem\Cos\Plugins\FileUrl;
 
-/**
- * Class CosStorageServiceProvider
- */
 class CosStorageServiceProvider extends ServiceProvider
 {
     /**
@@ -29,11 +17,7 @@ class CosStorageServiceProvider extends ServiceProvider
         Storage::extend('cos', function () {
             $adapter = new CosAdapter(\config('filesystems.disks.cos'));
 
-            $filesystem = new Filesystem($adapter);
-            $filesystem->addPlugin(new FileUrl());
-            $filesystem->addPlugin(new FileSignedUrl());
-
-            return $filesystem;
+            return new Filesystem($adapter);
         });
     }
 
